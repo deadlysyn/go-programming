bec735c24779c70a0480fa2786faccff5e3f4fa6
Mike Hoskins <mrh@devopsdreams.io>
2020-09-10 23:58:40 -0400

    initial commit

---
+++ 002-declarations/anything.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	b, _ := fmt.Println("Hello everyone")
+	fmt.Println(b)
+
+	// short delcaration
+	x := 42
+	// fun with expressions
+	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
+	y := 100 + 24
+	fmt.Println(y)
+}
