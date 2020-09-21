16e2075567c2a0ebc596968b9a1cf1ac15da23d5
Mike Hoskins <mrh@devopsdreams.io>
2020-09-20 21:49:09 -0400

    initial commit

---
+++ 058/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	q := make(chan int)
+	c := gen(q)
+
+	receive(c, q)
+
+	fmt.Println("about to exit")
+}
+
+func gen(q chan<- int) <-chan int {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		q <- 255
+	}()
+
+	return c
+}
+
+func receive(c, q <-chan int) {
+	for {
+		select {
+		case v := <-c:
+			fmt.Println("v value:", v)
+		case <-q:
+			return
+		}
+	}
+}
