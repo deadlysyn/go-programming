d1f8cf167c6eea3cd8b8948042b122c750ed4743
Mike Hoskins <mrh@devopsdreams.io>
2020-09-20 20:31:47 -0400

    fix another channel direction issue

--- 056/main.go
+++ 056/main.go
@@ -3,15 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	// error: send only channel can't receive on line 13
-	// cs := make(chan<- int)
-	cs := make(chan int)
+	// // error: send only channel can't receive on line 13
+	// // cs := make(chan<- int)
+	// cs := make(chan int)
+	// go func() {
+	// 	cs <- 42
+	// }()
+	// fmt.Println(<-cs)
+	// fmt.Printf("------\n")
+	// fmt.Printf("cs\t%T\n", cs)
 
+	// error: receive only channel can't send on line 20
+	// cr := make(<-chan int)
+	cr := make(chan int)
 	go func() {
-		cs <- 42
+		cr <- 42
 	}()
-	fmt.Println(<-cs)
+	fmt.Println(<-cr)
 
 	fmt.Printf("------\n")
-	fmt.Printf("cs\t%T\n", cs)
+	fmt.Printf("cr\t%T\n", cr)
 }
