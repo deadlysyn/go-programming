ec5cadd817eb86ed491609766f80b475dd77aae2
Mike Hoskins <mrh@devopsdreams.io>
2020-09-21 23:16:45 -0400

    initial commit

---
+++ 065/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		return 0, sqrtError{
+			lat:  "42.42",
+			long: "-42.42",
+			err:  fmt.Errorf("Invalid value: %v", f),
+		}
+	}
+	return 42, nil
+}
