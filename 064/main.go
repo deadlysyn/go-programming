2d8a38e100b09584eed7eba3129236986a24d511
Mike Hoskins <mrh@devopsdreams.io>
2020-09-21 22:38:19 -0400

    initial commit

---
+++ 064/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type customErr struct {
+	ErrorString string
+	ReturnValue int
+	Fatal       bool
+}
+
+// implement error interface
+func (c *customErr) Error() string {
+	return fmt.Sprintf("Custom error: %v (exit %v, fatal == %v)", c.ErrorString, c.ReturnValue, c.Fatal)
+}
+
+func foo(e error) {
+	fmt.Printf("%v satisfies the error interface\n", e)
+}
+
+func main() {
+	c := customErr{
+		ErrorString: "Something went wrong",
+		ReturnValue: 255,
+		Fatal:       true,
+	}
+
+	foo(&c)
+	fmt.Println(c)
+	fmt.Printf("%T\n", c)
+}
